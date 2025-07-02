5a738aba43cf47fe0c4fed54e65a999b9efcec51
kimaiyo <meshackkimaiyo5@gmail.com>
2025-07-02 14:36:25 +0300

    Refactor nodemon configuration and initialize Express server with dotenv integration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,22 @@
+import dotenv from 'dotenv';
+import express from 'express';
 
 
-console.log("Hello, World!");
+
+dotenv.config();
+
+const app = express();
+
+
+// middleware
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+const  port = process.env.PORT 
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
