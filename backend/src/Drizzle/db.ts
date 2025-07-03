bb056c8a509c237966111104c585b5c52f3172b5
kimaiyo <meshackkimaiyo5@gmail.com>
2025-07-03 12:30:01 +0300

    feat:finished migrations and seeding the database

--- backend/src/Drizzle/db.ts
+++ backend/src/Drizzle/db.ts
@@ -1,34 +1,29 @@
-
-import { connect } from "http2";
-import { Client } from "pg";
+import "dotenv/config";
+import {Client} from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
-import dotenv from "dotenv";
-dotenv.config();
 
 
-
-export const client = new Client ({
-    connectionString: process.env.DATABASE_URL 
+export const client = new Client({
+  connectionString: process.env.DATABASE_URL as string,
+  
 });
+export const main = async () => {
+    await client.connect();
+    console.log("Connected to the database successfully");
 
-const main = async () => {
-    try {
-        await client.connect();
-        console.log("Connected to the database successfully");
-    } catch (error) {
-        console.error("Error connecting to the database:", error);
-    }
-}
-main().catch((error) => {
-    console.error("Error in main function:", error);
-}).finally(() => {
-    client.end();
-});
-
-const db =drizzle(client, {
-    schema: {},
-    logger: true,
-});
-export default db;
     
+}
+main().catch((err) => {
+    console.error("Error connecting to the database:", err);
+    process.exit(1);
+});
+
+const db = drizzle(client,
+    {
+        schema: schema,
+        logger: true, 
+    }
+)
+
+export default db;
