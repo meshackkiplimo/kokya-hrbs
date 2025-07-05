2f7d558c8e629907b1df061e57a00e925e2b37ae
kimaiyo <meshackkimaiyo5@gmail.com>
2025-07-05 21:54:25 +0300

    feat: add Navbar component and integrate daisyUI for styling; update package.json and pnpm-lock.yaml for new dependency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-amber-900' >setup</h1>
-    </>
+    <Navbar />
+ </>
   )
 }
 
